cac35432a2c45d6f310b8534bf8d4377bd7a2bb5
Lao5 <i3town@163.com>
2023-01-20 23:04:52 +0800

    mod

--- pages/year/2023/script_text.js
+++ pages/year/2023/script_text.js
@@ -1,12 +1,27 @@
 'use strict';
 
+(function (doc, win) {
+	
 
+	var docEl = doc.documentElement;
+	var resizeEvt = 'onorientationchange' in window ? 'onorientationchange' : 'resize';
+	var recalc = function () {
+		var clientWidth = docEl.clientWidth;
+		if (!clientWidth) return;
+		if(clientWidth>=750){
+			docEl.style.fontSize = '100px';
+		}else{
+			docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+		}
+	};
 
-// const QRCode = require("./qrcode.min");
+	if (!doc.addEventListener) return;
+	win.addEventListener(resizeEvt, recalc, false);
+	
 
-var app = {
+	var app = {
 
-	chars: [
+		words: [
 			'',
 			'日月牵衣袖',
 			'春秋抚眉头',
@@ -20,6 +35,7 @@ var app = {
 			'跨海挥川',
 			'衣袂带霜',
 			'始终胸襟坦荡荡',
+			'',
 			'',
 			'',
 			'',
@@ -69,241 +85,302 @@ var app = {
 			'',
 			'',
 			'',
-			'最后 道一声',
-			'某某某 新年快乐',
+			'某某某',
+			'遥遥之愿',
+			'诚与君念',
+			'2023',
 			'祝您',
 			'身体健康',
-			'阖家欢乐',
-			'万事如意'
-	],
+			'家兴百和',
+			'万福骈臻',
+			'喜乐平安',
+			'诸事顺遂'
+		],
 
-	
+  		init: function () {
 
-  init: function () {
-	app.charIndex = -1;
+			//recalc();
+			app.fromName = new URL(window.location.href).searchParams.get('from');
+			app.toName = new URL(window.location.href).searchParams.get('to');
+			console.log("from = ",app.fromName);
+			console.log("to = ",app.toName);
 
-	app.zIndexTop = 999;
-	app.zIndexBottom = 0;
-	
-	// app.qrcodeImg = null;
-	
-	app.pageStart = document.getElementById('div-start');
-	app.pageStart.style.zIndex = app.zIndexTop;
-
-	app.pageShare = document.getElementById('div-share');
-	app.pageShare.style.zIndex = app.zIndexBottom;
-
-	app.pageDonate = document.getElementById('div-donate');
-	app.pageDonate.style.zIndex = app.zIndexBottom;
-
-	app.pageBtns = document.getElementById('div-btns');
-	app.pageBtns.style.zIndex = app.zIndexBottom;
-
-
-	app.btnStart = document.getElementById('btn-start');
-	app.btnStart.addEventListener('click', app.onBtnStart);
-
-	app.btnAgain = document.getElementById('btn-again');
-	app.btnAgain.addEventListener('click', app.onBtnAgain);
-
-	app.btnShare = document.getElementById('btn-share');
-	app.btnShare.addEventListener('click', app.onBtnShare);
-
-	app.btnDonate = document.getElementById('btn-donate');
-	app.btnDonate.addEventListener('click', app.onBtnDonate);
-
-	app.btnCloseDonate = document.getElementById('btn-Close-Donate');
-	app.btnCloseDonate.addEventListener('click', app.onBtnCloseDonate);
-
-	app.btnGenQRCode = document.getElementById('btn-Gen-QRCode');
-	app.btnGenQRCode.addEventListener('click', app.onBtnGenQRCode);
-
-	app.btnReGenQRCode = document.getElementById('btn-ReGen-QRCode');
-	app.btnReGenQRCode.addEventListener('click', app.onBtnCloseSharePage);
-
-	app.qrOutput = document.getElementById('output');
-
-	app.inputFrom = document.getElementById('input-from');
-	app.inputTo = document.getElementById('input-to');
-
-	//app.inputFrom.addEventListener('keyup',app.onInputKeyup);
-
-	app.popwindow = document.getElementById('popupwindow');
-
-	app.qrcode = new QRCode(app.qrOutput,{
-		colorDark:"#000000",
-		colorLight:"#ffffff",
-		correctLevel:QRCode.CorrectLevel.H
-	});
+			app.words[63] = app.toName;
+			app.words[66] = app.fromName + "祝您";
 
 
 
+			app.charIndex = -1;
 
-	// btnStart.onclick =function(){
-	// 	console.log('showMainScene');
-	// }
-	//btnStart.onclick = showMainScene;
-    // app.container = document.createElement('div');
-    // app.container.className = 'animation-container';
-    // document.body.appendChild(app.container);
-    // window.setInterval(app.add, 2000);
-  },
-
-  	onBtnStart:function () {
-		console.log('onBtnStart');
-		//var divStart = document.getElementById('div-start');
-		app.pageStart.classList.add('out');
-
-		app.pageStart.style.zIndex = app.zIndexBottom;
-
-		app.pageBtns.style.zIndex = app.zIndexTop;
-
-		app.container = document.createElement('div');
-		app.container.className = 'animation-container';
-		document.body.appendChild(app.container);
-		window.setInterval(app.add, 2000);
-
-	},
-
-	onBtnAgain:function () {
-		console.log('onBtnAgain');
-		
-		// app.popwindow.classList.toggle('pop-up');
-		// app.popwindow.classList.toggle('away');
-		// window.setTimeout(function(){
-		// 	app.popwindow.classList.toggle('out');
-		// },250);
-	},
-
-	onBtnShare:function () {
-		console.log('onBtnShare');
-
-		app.pageShare.classList.toggle('pop-up');
-		app.pageShare.classList.toggle('away');
-		window.setTimeout(function(){
-			app.pageShare.classList.toggle('out');
-		},250);
-
-		app.pageShare.style.zIndex = app.zIndexTop;
-
-		app.pageBtns.style.zIndex = app.zIndexBottom;
-	},
-
-	onBtnDonate:function () {
-		console.log('onBtnDonate');
-
-		app.pageDonate.classList.toggle('pop-up');
-		app.pageDonate.classList.toggle('away');
-		window.setTimeout(function(){
-			app.pageDonate.classList.toggle('out');
-		},250);
-
-		app.pageDonate.style.zIndex = app.zIndexTop;
-		app.pageBtns.style.zIndex = app.zIndexBottom;
-	},
-
-	onBtnCloseDonate:function () {
-		console.log('onBtnCloseDonate');
-
-		app.pageDonate.classList.toggle('pop-up');
-		app.pageDonate.classList.toggle('away');
-		window.setTimeout(function(){
-			app.pageDonate.classList.toggle('out');
-		},250);
-
-		app.pageDonate.style.zIndex = app.zIndexBottom;
-		app.pageBtns.style.zIndex = app.zIndexTop;
-	},
-
-	onBtnGenQRCode:function(){
-		console.log('onBtnGenQRCode');
-
-		app.qrcode.clear();
-
-		let txtFrom = app.inputFrom.value;
-		let txtTo = app.inputTo.value;
-		let urlTxt = "https://www.baidu.com?from="+txtFrom+"&to="+txtTo;
-		// let qrcode = new QRCode("output",{
-		// 	colorDark:"#000000",
-		// 	colorLight:"#ffffff",
-		// 	correctLevel:QRCode.CorrectLevel.H
-		// });
-
-		app.qrcode.makeCode(urlTxt);
-		app.qrOutput.style.opacity = 1;
-	},
-
-	onBtnCloseSharePage:function(){
-		console.log('onBtnCloseSharePage');
-
-		app.qrcode.clear();
-		app.inputFrom.value = '';
-		app.inputTo.value = '';
-
-		app.qrOutput.style.opacity = 0;
-
-		app.pageShare.classList.toggle('pop-up');
-		app.pageShare.classList.toggle('away');
-		window.setTimeout(function(){
-			app.pageShare.classList.toggle('out');
-		},250);
-
-		app.pageShare.style.zIndex = app.zIndexBottom;
-
-		app.pageBtns.style.zIndex = app.zIndexTop;
-		
+			app.zIndexTop = 999;
+			app.zIndexBottom = 0;
 			
-	},
+			// app.qrcodeImg = null;
+			
+			app.pageStart = document.getElementById('div-start');
+			app.pageStart.style.zIndex = app.zIndexTop;
 
-	// onInputKeyup:function(e)
-	// {
-	// 	console.log("onInputKeyup");
-	// 	var value = e.target.value;
-	// 	console.log("value = ",value);
+			app.pageShare = document.getElementById('div-share');
+			app.pageShare.style.zIndex = app.zIndexBottom;
 
-	// 	app.changeInputLabelStyle(e.target,value);
-	// },
+			app.pageDonate = document.getElementById('div-donate');
+			app.pageDonate.style.zIndex = app.zIndexBottom;
 
-	// changeInputLabelStyle:function(ele,val){
-	// 	var label = ele.previousElementSibling;
-	// 	//console.log("label = ",label);
-		
-		
-	// },
+			app.pageBtns = document.getElementById('div-btns');
+			app.pageBtns.style.zIndex = app.zIndexBottom;
+
+
+			app.btnStart = document.getElementById('btn-start');
+			app.btnStart.addEventListener('click', app.onBtnStart);
+			app.btnStart.innerHTML = "一条来自" + app.fromName + "的新年祝福";
+
+			app.btnAgain = document.getElementById('btn-again');
+			app.btnAgain.addEventListener('click', app.onBtnAgain);
+			app.btnAgain.disabled = true;
+			app.btnAgain.innerHTML = "祝福语播放中";
+			//app.btnAgain.style.opacity = 0;
+
+			app.btnShare = document.getElementById('btn-share');
+			app.btnShare.addEventListener('click', app.onBtnShare);
+
+			app.btnDonate = document.getElementById('btn-donate');
+			app.btnDonate.addEventListener('click', app.onBtnDonate);
+
+			app.btnCloseDonate = document.getElementById('btn-Close-Donate');
+			app.btnCloseDonate.addEventListener('click', app.onBtnCloseDonate);
+
+			app.btnGenQRCode = document.getElementById('btn-Gen-QRCode');
+			app.btnGenQRCode.addEventListener('click', app.onBtnGenQRCode);
+
+			app.btnReGenQRCode = document.getElementById('btn-ReGen-QRCode');
+			app.btnReGenQRCode.addEventListener('click', app.onBtnCloseSharePage);
+
+			app.qrOutput = document.getElementById('output');
+
+			app.inputFrom = document.getElementById('input-from');
+			app.inputTo = document.getElementById('input-to');
+
+			//app.inputFrom.addEventListener('keyup',app.onInputKeyup);
+
+			app.popwindow = document.getElementById('popupwindow');
+
+			// app.qrcode = new QRCode(app.qrOutput,{
+			// 	colorDark:"#000000",
+			// 	colorLight:"#ffffff",
+			// 	correctLevel:QRCode.CorrectLevel.H
+			// });
+
+			// btnStart.onclick =function(){
+			// 	console.log('showMainScene');
+			// }
+			//btnStart.onclick = showMainScene;
+			// app.container = document.createElement('div');
+			// app.container.className = 'animation-container';
+			// document.body.appendChild(app.container);
+			// window.setInterval(app.add, 2000);
+		},
+
+		onBtnStart:function () {
+			console.log('onBtnStart');
+			//var divStart = document.getElementById('div-start');
+			app.pageStart.classList.add('out');
+
+			app.pageStart.style.zIndex = app.zIndexBottom;
+
+			app.pageBtns.style.zIndex = app.zIndexTop;
+
+			app.container = document.createElement('div');
+			app.container.className = 'animation-container';
+			document.body.appendChild(app.container);
+			window.setInterval(app.add, 2000);
+
+		},
+
+		onBtnAgain:function () {
+			console.log('onBtnAgain');
+			app.charIndex = -1;
+			// app.popwindow.classList.toggle('away');
+			// window.setTimeout(function(){
+			// 	app.popwindow.classList.toggle('out');
+			// },250);
+		},
+
+		onBtnShare:function () {
+			console.log('onBtnShare');
+
+			app.pageShare.classList.toggle('pop-up');
+			app.pageShare.classList.toggle('away');
+			window.setTimeout(function(){
+				app.pageShare.classList.toggle('out');
+			},250);
+
+			app.pageShare.style.zIndex = app.zIndexTop;
+
+			app.pageBtns.style.zIndex = app.zIndexBottom;
+		},
+
+		onBtnDonate:function () {
+			console.log('onBtnDonate');
+
+			app.pageDonate.classList.toggle('pop-up');
+			app.pageDonate.classList.toggle('away');
+			window.setTimeout(function(){
+				app.pageDonate.classList.toggle('out');
+			},260);
+
+			app.pageDonate.style.zIndex = app.zIndexTop;
+			app.pageBtns.style.zIndex = app.zIndexBottom;
+		},
+
+		onBtnCloseDonate:function () {
+			console.log('onBtnCloseDonate');
+
+			app.pageDonate.classList.toggle('pop-up');
+			app.pageDonate.classList.toggle('away');
+			window.setTimeout(function(){
+				app.pageDonate.classList.toggle('out');
+			},260);
+
+			app.pageDonate.style.zIndex = app.zIndexBottom;
+			app.pageBtns.style.zIndex = app.zIndexTop;
+		},
+
+		onBtnGenQRCode:function(){
+			console.log('onBtnGenQRCode');
+
+
+			let txtFrom = app.inputFrom.value;
+			let txtTo = app.inputTo.value;
+
+			console.log("txtFrom=",txtFrom);
+			console.log("txtTo=",txtTo);
+
+			if(txtFrom == "" || txtTo == "")
+			{
+				console.log("名字未填写");
+				app.btnGenQRCode.innerHTML = "两个名字都要填写";
+				window.setTimeout(function(){
+					app.btnGenQRCode.innerHTML = "生成祝福二维码";
+				},2000);
+				return;
+			}
+
+			if(app.qrcode != undefined)
+			{
+				app.qrcode.clear();
+			}
+			else
+			{
+				app.qrcode = new QRCode(app.qrOutput,{
+					colorDark:"#000000",
+					colorLight:"#ffffff",
+					correctLevel:QRCode.CorrectLevel.H
+				});
 	
+				app.qrcode.clear();
+			}
+
+			
+
+			
 
 
-  add: function () {
-    var element = document.createElement('span');
-    app.container.appendChild(element);
-    app.animate(element);
-  },
+			let urlTxt = "https://www.baidu.com?from="+txtFrom+"&to="+txtTo;
+			// let qrcode = new QRCode("output",{
+			// 	colorDark:"#000000",
+			// 	colorLight:"#ffffff",
+			// 	correctLevel:QRCode.CorrectLevel.H
+			// });
 
-	animate: function (element) {
-		app.charIndex = app.charIndex + 1;
-	  	if(app.charIndex>=app.chars.length)
-	  	{
-			app.charIndex = app.chars.length - 1;
-		  	return;
-	  	}
-    //var character = app.chars[Math.floor(Math.random() * app.chars.length)];
-	var character = app.chars[app.charIndex];
-    //var duration = Math.floor(Math.random() * 15) + 1;
-	var duration = 16;
-    var offset = Math.floor(Math.random() * (50 - duration * 2)) + 3;
-    //var size = 10 + (15 - duration);
-	var size = 30;
-    element.style.cssText = 'color: #c9a063;text-shadow: 0 0 8px rgba(255, 255, 255, 0.3);right:'+offset+'vw; font-size:'+size+'px;animation-duration:'+duration+'s';
-	//element.style.cssText = 'color: #fee;text-shadow: 0 -40px 100px, 0 0 2px, 0 0 1em #ff4444, 0 0 0.5em #ff4444, 0 0 0.1em #ff4444, 0 10px 3px #000;right:'+offset+'vw; font-size:'+size+'px;animation-duration:'+duration+'s';
-    
-	element.innerHTML = character;
-    window.setTimeout(app.remove, duration * 1000, element);
-  },
+			app.qrcode.makeCode(urlTxt);
+			app.qrOutput.style.opacity = 1;
+		},
 
-  remove: function (element) {
-    element.parentNode.removeChild(element);
-  },
+		onBtnCloseSharePage:function(){
+			console.log('onBtnCloseSharePage');
 
-};
+			console.log('app.qrcode',app.qrcode);
+			if(app.qrcode != undefined)
+			{
+				app.qrcode.clear();
+			}
+			
+			app.inputFrom.value = '';
+			app.inputTo.value = '';
 
-document.addEventListener('DOMContentLoaded', app.init);
+			app.qrOutput.style.opacity = 0;
+
+			app.pageShare.classList.toggle('pop-up');
+			app.pageShare.classList.toggle('away');
+			window.setTimeout(function(){
+				app.pageShare.classList.toggle('out');
+			},250);
+
+			app.pageShare.style.zIndex = app.zIndexBottom;
+
+			app.pageBtns.style.zIndex = app.zIndexTop;
+			
+				
+		},
+
+		// onInputKeyup:function(e)
+		// {
+		// 	console.log("onInputKeyup");
+		// 	var value = e.target.value;
+		// 	console.log("value = ",value);
+
+		// 	app.changeInputLabelStyle(e.target,value);
+		// },
+
+		// changeInputLabelStyle:function(ele,val){
+		// 	var label = ele.previousElementSibling;
+		// 	//console.log("label = ",label);
+			
+			
+		// },
+		
+
+
+		add: function () {
+			var element = document.createElement('span');
+			app.container.appendChild(element);
+			app.animate(element);
+		},
+
+		animate: function (element) {
+			app.charIndex = app.charIndex + 1;
+			if(app.charIndex>=app.words.length)
+			{
+				app.charIndex = app.words.length - 1;
+				return;
+			}
+			//var character = app.words[Math.floor(Math.random() * app.words.length)];
+			var character = app.words[app.charIndex];
+			//var duration = Math.floor(Math.random() * 15) + 1;
+			var duration = 16;
+			var offset = Math.floor(Math.random() * (50 - duration * 2)) + 3;
+			//var size = 10 + (15 - duration);
+			var size = 2.5;
+			element.style.cssText = 'color: #c9a063;text-shadow: 0 0 8px rgba(255, 255, 255, 0.3);right:'+offset+'vw; font-size:'+size+'rem;animation-duration:'+duration+'s';
+			//element.style.cssText = 'color: #fee;text-shadow: 0 -40px 100px, 0 0 2px, 0 0 1em #ff4444, 0 0 0.5em #ff4444, 0 0 0.1em #ff4444, 0 10px 3px #000;right:'+offset+'vw; font-size:'+size+'px;animation-duration:'+duration+'s';
+			
+			element.innerHTML = character;
+			window.setTimeout(app.remove, duration * 1000, element);
+		},
+
+		remove: function (element) {
+			element.parentNode.removeChild(element);
+			if(app.charIndex >= app.words.length-1)
+			{
+				//app.btnAgain.style.opacity = 1;
+				app.btnAgain.disabled = false;
+				app.btnAgain.innerHTML = "祝福语";
+			}
+		},
+
+	};
+
+	
+	doc.addEventListener('DOMContentLoaded', app.init);
+
+})(document, window);
